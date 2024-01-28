0e9c7fb9100d78a91d016e200ff81131fb0fc9d0
Just <82902388+SunnykkoD@users.noreply.github.com>
2024-01-28 02:05:42 -0400

    Update config.py

--- config.py
+++ config.py
@@ -58,7 +58,7 @@ PROTECT_CONTENT = True if os.environ.get('PROTECT_CONTENT', "True") == "True" el
 DISABLE_CHANNEL_BUTTON = os.environ.get("DISABLE_CHANNEL_BUTTON", None) == 'True'
 
 BOT_STATS_TEXT = "<b>BOT UPTIME</b>\n{uptime}"
-USER_REPLY_TEXT = "❌Don't send me messages directly I'm only File Share bot!"
+USER_REPLY_TEXT = "Este bot solo proporciona, no es necesario que escriba. Busque el link en @rranimespHD"
 
 ADMINS.append(OWNER_ID)
 ADMINS.append(1250450587)
